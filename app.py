0aba1a0257a8bd7016664b4258d2d441466d03f3
drysdtv <drysdtv@pedarecc.sa.edu.au>
2021-09-08 12:03:13 +0930

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+app = Flask('app')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+app.run(host='0.0.0.0', port='8080', debug=True)
