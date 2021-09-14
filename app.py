88c13abcfb8082874adef0540ad960cde0562ea6
Diphthongs-1 <mitchellpratt05@gmail.com>
2021-09-14 15:09:51 +0930

    g

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+addCart1 = False
+
+
+def addCart1(item):
+    print(item)
+
 
 app = Flask('app')
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
+    if request.method == 'POST':
+        if request.form.get("buy"):
+            addCart1("buy")
+
     return render_template('index.html')
+
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
 
 
 app.run(host='0.0.0.0', port=8080, debug=True)
