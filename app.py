acec3829bb2bcdf9c2f8c06772fff8208e1f629f
Diphthongs1 <mitchellpratt05@gmail.com>
2021-10-10 21:16:05 +1030

    y

--- app.py
+++ app.py
@@ -53,6 +53,20 @@ def cart():
     global quantity2
     global quantity3
     global quantity4
+    if request.method == 'POST':
+        if request.form.get("q1"):
+            u1 = int(request.form["q1"])
+            quantity1 = u1
+        if request.form.get("q2"):
+            u1 = int(request.form["q2"])
+            quantity2 = u1
+        if request.form.get("q3"):
+            u1 = int(request.form["q3"])
+            quantity3 = u1
+        if request.form.get("q4"):
+            u1 = int(request.form["q4"])
+            quantity4 = u1
+
     if request.form.get('watch1'):
         quantity1 = 0
         item = request.form.get('watch1')
@@ -62,22 +76,22 @@ def cart():
     if request.form.get('watch2'):
         quantity2 = 0
         item = request.form.get('watch2')
-        res = make_response(render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4,quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4))
+        res = make_response(render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4, quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4))
         res.set_cookie(item, str(quantity2), max_age=0)
         return res
     if request.form.get('watch3'):
         quantity3 = 0
         item = request.form.get('watch3')
-        res = make_response(render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4,quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4))
-        res.set_cookie(item, str(quantity3), max_age=0)
+        res = make_response(render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4, quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4))
+        res.set_cookie(item, expires=0)
         return res
     if request.form.get('watch4'):
         quantity4 = 0
         item = request.form.get('watch4')
-        res = make_response(render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4,quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4))
+        res = make_response(render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4, quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4))
         res.set_cookie(item, str(quantity4), max_age=0)
         return res
-    return render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4,quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4)
+    return render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4, quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4)
 
 
 app.run(host='0.0.0.0', port=5000, debug=True)
