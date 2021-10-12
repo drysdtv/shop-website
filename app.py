2cd4108a63d1d1130c52e4a6f2ea6f6e3538e39f
Diphthongs-1 <mitchellpratt05@gmail.com>
2021-10-13 09:02:59 +1030

    g

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, make_response
+from flask import Flask, render_template, request, redirect, url_for, make_response, flash
 import sqlite3 as sql
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length, NoneOf, AnyOf
@@ -72,8 +72,12 @@ class SignUp(FlaskForm):
     password = PasswordField('password', validators=[InputRequired(), Length(min=4, max=20,
                                                                              message='Please enter a password between 4 and 20 characters')])
 
+@app.route('/')
+def starting():
+    return redirect(url_for("index"))
 
-@app.route('/', methods=['POST', 'GET'])
+
+@app.route('/index', methods=['POST', 'GET'])
 def index():
     global loggedIn
     global quantity1
@@ -84,28 +88,28 @@ def index():
         if request.form.get('watch1'):
             quantity1 += 1
             item = request.form.get('watch1')
-            res = make_response(render_template('index.html'))
+            res = make_response(redirect(url_for("index")))
             res.set_cookie(item, str(quantity1))
             dbh.addCart(str(item), 599, quantity1)
             return res
         if request.form.get('watch2'):
             item = request.form.get('watch2')
             quantity2 += 1
-            res = make_response(render_template('index.html'))
+            res = make_response(redirect(url_for("index")))
             res.set_cookie(item, str(quantity2))
             dbh.addCart(item, 599, quantity2)
             return res
         if request.form.get('watch3'):
             quantity3 += 1
             item = request.form.get('watch3')
-            res = make_response(render_template('index.html'))
+            res = make_response(redirect(url_for("index")))
             res.set_cookie(item, str(quantity3))
             dbh.addCart(item, 599, quantity3)
             return res
         if request.form.get('watch4'):
             quantity4 += 1
             item = request.form.get('watch4')
-            res = make_response(render_template('index.html'))
+            res = make_response(redirect(url_for("index")))
             res.set_cookie(item, str(quantity4))
             dbh.addCart(item, 599, quantity4)
             return res
@@ -116,6 +120,7 @@ def index():
 
 @app.route('/cart', methods=['POST', 'GET'])
 def cart():
+    global loggedIn
     x1 = request.cookies.get('watch1')
     x2 = request.cookies.get('watch2')
     x3 = request.cookies.get('watch3')
@@ -142,31 +147,31 @@ def cart():
         quantity1 = 0
         item = request.form.get('watch1')
         res = make_response(
-            render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4, quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4))
+            render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4, quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4, loggedIn=loggedIn))
         res.set_cookie(item, str(quantity1), max_age=0)
         return res
     if request.form.get('watch2'):
         quantity2 = 0
         item = request.form.get('watch2')
         res = make_response(
-            render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4, quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4))
+            render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4, quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4, loggedIn=loggedIn))
         res.set_cookie(item, str(quantity2), max_age=0)
         return res
     if request.form.get('watch3'):
         quantity3 = 0
         item = request.form.get('watch3')
         res = make_response(
-            render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4, quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4))
+            render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4, quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4, loggedIn=loggedIn))
         res.set_cookie(item, expires=0)
         return res
     if request.form.get('watch4'):
         quantity4 = 0
         item = request.form.get('watch4')
         res = make_response(
-            render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4, quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4))
+            render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4, quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4, loggedIn=loggedIn))
         res.set_cookie(item, str(quantity4), max_age=0)
         return res
-    return render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4, quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4)
+    return render_template('cart.html', x1=x1, x2=x2, x3=x3, x4=x4, quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4, loggedIn=loggedIn)
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -193,15 +198,9 @@ def login():
             fail = True
             return render_template('login.html', fail=fail, form=form)
         else:
-            print('1')
             us = load_user(user[0])
-            print(form.username.data)
-            print(us.username)
-            print(form.password.data)
-            print(us.password)
             if form.username.data == us.username:
                 if form.password.data == us.password:
-                    print('2')
                     login_user(us, remember=form.remember.data)
                     loggedIn = True
                     return render_template('cart.html', loggedIn=loggedIn, name=us.username, x1=x1, x2=x2, x3=x3, x4=x4, quantity1=quantity1, quantity2=quantity2, quantity3=quantity3, quantity4=quantity4)
